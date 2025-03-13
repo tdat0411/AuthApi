c59d3565b7212a9804f7e2cd8c7f11eaa73039a9
tdat0411 <dattrantien654@gmail.com>
2025-03-13 13:51:09 +0700

    AuthApi

--- obj/Debug/net9.0/AuthApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/AuthApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0d2f2e51f8058e2b583a53ebd5dc8d3a7274c54")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
